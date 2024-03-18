8fe86327b6538119759db0eb570c4c9c5554f311
kamlesh <kamleshchandel17@gmail.com>
2024-03-18 19:07:56 +0000

    first commit

---
+++ src/components/MemeGenerator/styledComponents.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const CustomDiv = styled.div`
+  background-image: url(" ${props => props.imageUrl} ");
+  height: 100vh;
+`
+export const CustomPara = styled.p`
+  color: #ffffff;
+  font-size: ${props => 
+    props.fontSize
+  };
+`
